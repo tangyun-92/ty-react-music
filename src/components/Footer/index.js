a6272ec574833c73627777a4e9d58e968e431650
tangyun <tangyun1992@qq.com>
2021-02-18 23:22:27 +0800

    完成Footer组件开发

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,64 @@
 /*
- * @Author: 唐云 
- * @Date: 2021-02-18 15:41:14 
+ * @Author: 唐云
+ * @Date: 2021-02-18 15:41:14
  * @Last Modified by: 唐云
- * @Last Modified time: 2021-02-18 15:42:41
+ * @Last Modified time: 2021-02-18 23:11:49
  * 底部组件
  */
-import React, { memo } from 'react'
+import React, { memo, Fragment } from 'react'
+
+import { FooterWrapper, FooterLeft, FooterRight } from './style'
+import { footerLinks, footerImages } from '@/common/local-data.js'
 
 export default memo(function TYFooter() {
-  return <div>TYFooter</div>
+  return (
+    <FooterWrapper>
+      <div className="content wrap-v2">
+        <FooterLeft>
+          <div className="footer-item">
+            {footerLinks.map((item, index) => {
+              return (
+                <Fragment key={item.title}>
+                  <a href={item.link} target="_blank" rel="noreferrer">
+                    {item.title}
+                  </a>
+                  <span>|</span>
+                </Fragment>
+              )
+            })}
+          </div>
+          <ul>
+            <li>
+              网易公司版权所有©️1997-2021
+              杭州乐读科技有限公司运营：浙网文[2018]3506-263号
+            </li>
+            <li>
+              违法和不良信息举报电话：0571-89853516 举报邮箱：ncm5990@163.com
+            </li>
+            <li>
+              粤B2-20090191-18 工业和信息化部备案管理系统网站 浙公安备
+              2201902002564号
+            </li>
+          </ul>
+        </FooterLeft>
+        <FooterRight>
+          {footerImages.map((item, index) => {
+            return (
+              <li className="item" key={item.link}>
+                <a
+                  className="link"
+                  href={item.link}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  {' '}
+                </a>
+                <span className="title">{item.title}</span>
+              </li>
+            )
+          })}
+        </FooterRight>
+      </div>
+    </FooterWrapper>
+  )
 })
---
+++ src/components/Footer/style.js
@@ -0,0 +1,91 @@
+import styled from 'styled-components'
+
+export const FooterWrapper = styled.div`
+  height: 172px;
+  border-top: 1px solid #d3d3d3;
+  background-color: #f2f2f2;
+
+  .content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`
+
+export const FooterLeft = styled.div`
+  padding-top: 15px;
+  line-height: 24px;
+  color: #666;
+
+  .footer-item {
+
+    a {
+      color: #999;
+      position: relative;
+      top: 1px;
+    }
+  
+    span {
+      margin: 0 15px;
+    }
+  }
+
+`
+
+export const FooterRight = styled.ul`
+  display: flex;
+
+  .item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-right: 40px;
+
+    .link {
+      display: block;
+      width: 50px;
+      height: 45px;
+      background-image: url(${require('@/assets/img/sprite_footer_02.png').default});
+      background-size: 110px 450px;
+    }
+
+    :nth-child(1) .link {
+      background-position: -60px -101px;
+    }
+    :nth-child(2) .link {
+      background-position: 0 0;
+    }
+    :nth-child(3) .link {
+      background-position: -60px -50px;
+    }
+    :nth-child(4) .link {
+      background-position: 0 -101px;
+    }
+
+    .title {
+      margin-top: 5px;
+      display: block;
+      width: 52px;
+      height: 10px;
+      background-image: url(${require('@/assets/img/sprite_footer_01.png').default});
+      background-size: 180px 100px;
+    }
+
+    :nth-child(1) .title {
+      background-position: -1px -90px;
+    }
+    :nth-child(2) .title {
+      background-position: 0 0;
+      margin-top: 7px;
+    }
+    :nth-child(3) .title {
+      background-position: 0 -54px;
+      margin-top: 6px;
+    }
+
+    :nth-child(4) .title {
+      background-position: -1px -72px;
+      margin-top: 6px;
+    }
+  }
+`
