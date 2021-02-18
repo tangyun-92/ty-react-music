f9de90cf8e52f987aaeb34bef13b84266042fcbf
tangyun <tangyun1992@qq.com>
2021-02-18 22:44:16 +0800

    完成Header组件开发

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,69 @@
 /*
- * @Author: 唐云 
- * @Date: 2021-02-18 15:41:47 
+ * @Author: 唐云
+ * @Date: 2021-02-18 15:41:47
  * @Last Modified by: 唐云
- * @Last Modified time: 2021-02-18 15:57:57
+ * @Last Modified time: 2021-02-18 22:41:17
  * 头部组件
  */
 import React, { memo } from 'react'
 import { NavLink } from 'react-router-dom'
+import classnames from 'classnames'
+import { Input } from 'antd'
+import { SearchOutlined } from '@ant-design/icons'
+
+import { headerLinks } from '@/common/local-data.js'
+import { HeaderWrapper, HeaderLeft, HeaderRight } from './style'
 
 export default memo(function TYHeader() {
-  return <div>
-    <NavLink to="/">发现音乐</NavLink>
-    <NavLink to="/mine">我的</NavLink>
-    <NavLink to="/friend">朋友</NavLink>
-  </div>
+  const showItem = (item, index) => {
+    if (index < 3) {
+      return (
+        <NavLink to={item.link}>
+          {item.title}
+          <i className="sprite_01 icon"></i>
+        </NavLink>
+      )
+    } else {
+      return (
+        <a href={item.link} target="_blank" rel="noopener noreferrer">
+          {item.title}
+        </a>
+      )
+    }
+  }
+
+  return (
+    <HeaderWrapper>
+      <div className="content wrap-v1">
+        <HeaderLeft>
+          <a href="#/" className="logo sprite_01">
+            网易云音乐
+          </a>
+          <div className="select-list">
+            {headerLinks.map((item, index) => {
+              return (
+                <div className={classnames('select-item')} key={item.title}>
+                  {showItem(item, index)}
+                </div>
+              )
+            })}
+          </div>
+        </HeaderLeft>
+        <HeaderRight>
+          <Input
+            className="search"
+            placeholder="音乐/视频/电台/用户"
+            prefix={<SearchOutlined />}
+          />
+          <div className="center">
+            创作者中心
+          </div>
+          <div className="login">
+            登录
+          </div>
+        </HeaderRight>
+      </div>
+      <div className="divider"></div>
+    </HeaderWrapper>
+  )
 })
