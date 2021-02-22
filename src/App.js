1a0b98feb47bcf24563acccf7af8f4447fe5deda
tangyun <tangyun1992@qq.com>
2021-02-22 21:41:46 +0800

    优化组件模版

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
  * @Author: 唐云
  * @Date: 2021-02-17 22:55:33
  * @Last Modified by: 唐云
- * @Last Modified time: 2021-02-22 21:39:56
+ * @Last Modified time: 2021-02-22 21:41:25
  */
 import React, { memo, Suspense } from 'react'
 import { renderRoutes } from 'react-router-config'
@@ -20,12 +20,12 @@ export default memo(function App() {
   return (
     <Provider store={store}>
       <HashRouter>
-        <TYHeader></TYHeader>
+        <TYHeader />
         <Suspense fallback={<div>page loading</div>}>
           {renderRoutes(routes)}
         </Suspense>
-        <TYFooter></TYFooter>
-        <TYAppPlayBar></TYAppPlayBar>
+        <TYFooter />
+        <TYAppPlayBar />
       </HashRouter>
     </Provider>
   )
