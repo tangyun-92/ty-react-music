d30643b27acb127403b03d28b57fd76b75c857e7
tangyun <tangyun1992@qq.com>
2021-02-22 22:18:10 +0800

    添加loading

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
  * @Author: 唐云
  * @Date: 2021-02-17 22:55:33
  * @Last Modified by: 唐云
- * @Last Modified time: 2021-02-22 21:41:25
+ * @Last Modified time: 2021-02-22 22:17:39
  */
 import React, { memo, Suspense } from 'react'
 import { renderRoutes } from 'react-router-config'
 import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { Spin } from 'antd'
 
 import routes from '@/router'
 import store from '@/store'
@@ -21,7 +22,13 @@ export default memo(function App() {
     <Provider store={store}>
       <HashRouter>
         <TYHeader />
-        <Suspense fallback={<div>page loading</div>}>
+        <Suspense
+          fallback={
+            <div className="example">
+              <Spin size="large" tip="Loading..." />
+            </div>
+          }
+        >
           {renderRoutes(routes)}
         </Suspense>
         <TYFooter />
