e7900292e27993b8189db386adabd0816a8a2eee
tangyun <tangyun1992@qq.com>
2021-03-01 16:27:32 +0800

    改变项目目录结构

--- src/router/index.js
+++ src/router/index.js
@@ -3,15 +3,15 @@ import { Redirect } from 'react-router-dom'
 
 const TYDiscover = React.lazy(() => import('@/pages/discover'))
 const TYRecommend = React.lazy(() =>
-  import('@/pages/discover/c-pages/recommend')
+  import('@/pages/discover/recommend')
 )
-const TYTopList = React.lazy(() => import('@/pages/discover/c-pages/top-list'))
+const TYTopList = React.lazy(() => import('@/pages/discover/top-list'))
 const TYPlayList = React.lazy(() =>
-  import('@/pages/discover/c-pages/play-list')
+  import('@/pages/discover/play-list')
 )
-const TYDjRadio = React.lazy(() => import('@/pages/discover/c-pages/dj-radio'))
-const TYArtist = React.lazy(() => import('@/pages/discover/c-pages/artist'))
-const TYAlbum = React.lazy(() => import('@/pages/discover/c-pages/album'))
+const TYDjRadio = React.lazy(() => import('@/pages/discover/dj-radio'))
+const TYArtist = React.lazy(() => import('@/pages/discover/artist'))
+const TYAlbum = React.lazy(() => import('@/pages/discover/album'))
 const TYPlayer = React.lazy(() => import('@/pages/player'))
 const TYFriend = React.lazy(() => import('@/pages/friend'))
 const TYMine = React.lazy(() => import('@/pages/mine'))
